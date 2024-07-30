8cb1cfe01eac0c1568248052c6ab0d98022a277e
hqhs <hqhs@users.noreply.github.com>
2024-07-31 00:13:41 +0400

    tera for templates

--- crates/service/src/lib.rs
+++ crates/service/src/lib.rs
@@ -1,14 +1,129 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    path::Path,
+    sync::{Arc, RwLock},
+};
+
+use axum::{
+    extract::{Request, State},
+    handler::HandlerWithoutStateExt,
+    middleware::{from_fn_with_state, Next},
+    response::{Html, IntoResponse, Response},
+    routing::{get, get_service},
+    Extension, Router, ServiceExt,
+};
+use hyper::StatusCode;
+use tera::Tera;
+use tower_http::services::ServeDir;
+
+#[derive(Debug, Clone)]
+pub enum AppError {
+    Internal(Arc<anyhow::Error>),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let mut response = match self {
+            AppError::Internal(ref a) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Something went wrong: {}", a),
+            )
+                .into_response(),
+        };
+        response.extensions_mut().insert(self);
+        response
     }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        AppError::Internal(Arc::new(err.into()))
+    }
+}
+
+type Template = Result<Html<String>, AppError>;
+type TemplateStorage = RwLock<Tera>;
+
+pub struct ServerState {
+    // pub diesel: DieselPool<diesel_async::
+    templates: TemplateStorage,
+}
+
+impl ServerState {
+    pub fn new(templates: Tera) -> Self {
+        let templates = RwLock::new(templates);
+        ServerState { templates }
+    }
+
+    pub fn render_template(
+        &self,
+        name: &str,
+        context: &tera::Context,
+    ) -> Result<String, AppError> {
+        let borrowed = self.templates.try_read().unwrap();
+        borrowed.render(name, context).map_err(|e| e.into())
+    }
+
+    #[allow(dead_code)]
+    pub fn reload_templates(&self) -> Result<(), AppError> {
+        #[cfg(feature = "reload")]
+        {
+            let mut borrowed = self.templates.try_write().unwrap();
+            return borrowed.full_reload().map_err(|e| e.into());
+        }
+        #[cfg(not(feature = "reload"))]
+        return Ok(());
+    }
+}
+
+#[derive(Clone)]
+struct Cx {
+    server: Arc<ServerState>,
+    request_id: String,
+}
+
+pub fn build_root_router<P: AsRef<Path>>(
+    server: Arc<ServerState>,
+    path: P,
+) -> Router {
+    let common_middleware =
+        from_fn_with_state(server, common_request_cx_middleware);
+    let serve_dir = ServeDir::new(path);
+
+    Router::new()
+        .route("/", get(root))
+        .nest_service("/static", serve_dir)
+        .route_layer(common_middleware)
+}
+
+async fn root(Extension(cx): Extension<Cx>) -> Template {
+    let template_cx = tera::Context::new();
+    let template = cx.server.render_template("home.jinja2", &template_cx)?;
+    Ok(Html(template))
+}
+
+async fn handle_404() -> (StatusCode, &'static str) {
+    (StatusCode::NOT_FOUND, "Not found")
+}
+
+async fn common_request_cx_middleware(
+    State(server): State<Arc<ServerState>>,
+    mut request: Request,
+    next: Next,
+) -> Response {
+    let request_id = String::new();
+    let cx = Cx { server, request_id };
+    request.extensions_mut().insert(cx);
+
+    let response = next.run(request).await;
+
+    response
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
 }
--- crates/service/src/main.rs
+++ crates/service/src/main.rs
@@ -1,11 +1,45 @@
-use models::Post;
-use service::add;
+use std::{path::PathBuf, sync::Arc};
 
-fn main() {
+use models::Post;
+use service::{add, build_root_router, ServerState};
+use tera::Tera;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
     let _post = Post {
         id: Some(0),
         title: "Hello".to_string(),
         body: "World".to_string(),
     };
+
+    let templates = {
+        let path: PathBuf =
+            [env!("CARGO_MANIFEST_DIR"), "templates"].iter().collect();
+        if !path.is_dir() {
+            anyhow::bail!(
+                "{} directory does not exist",
+                path.to_string_lossy()
+            );
+        }
+        let glob = format!("{}/**/*.jinja2", path.to_string_lossy());
+        let mut templates = Tera::new(&glob)?;
+        templates.autoescape_on(vec![".jinja2"]);
+        templates
+    };
+
+    let server = Arc::new(ServerState::new(templates));
+
+    let static_path: PathBuf =
+        [env!("CARGO_MANIFEST_DIR"), "static"].iter().collect();
+    let app = build_root_router(server, static_path);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+
     println!("Hello world! {}", add(3, 4));
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
