b4b49750565ccecfcfc592a52f70d32844f8abe1
hqhs <hqhs@users.noreply.github.com>
2024-07-31 03:10:34 +0400

    set default log level for tracing

--- crates/service/src/main.rs
+++ crates/service/src/main.rs
@@ -1,17 +1,23 @@
 use std::env;
 
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
 use service::setup_app;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
+    let subscriber =
+        FmtSubscriber::builder().with_max_level(Level::INFO).finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
     dotenvy::dotenv().ok();
 
     let database_url = env::var("DATABASE_URL")?;
     let app = setup_app(&database_url)?;
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
 
-    tracing::error!("serving requests...");
+    tracing::info!("serving requests...");
 
     axum::serve(listener, app).await?;
 
